f19b5819ea0da4ce1cc0f2fd6b9d739481b6512f
Ahsan-Sarbaz <geeksdotpk@gmail.com>
2025-10-03 19:16:44 +0500

    fix bug in platform title not being set

--- Platform.cpp
+++ Platform.cpp
@@ -8,7 +8,7 @@ Window::Window(int width, int height, const char* title)
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-	window = glfwCreateWindow(width, height, "Sokuban", nullptr, nullptr);
+	window = glfwCreateWindow(width, height, title, nullptr, nullptr);
 	glfwMakeContextCurrent(window);
 	glfwSwapInterval(1);
 }
